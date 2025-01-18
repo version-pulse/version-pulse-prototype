cedb22e9238ca73964c8b518bb4d75348b0eafae
kwonssshyeon <kwonsh4396@gmail.com>
2025-01-18 13:43:36 +0900

    Feat: define custom annotation

---
+++ src/v0/annotations/Api.java
@@ -0,0 +1,14 @@
+package v0.annotations;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Api {
+	String name() default "";
+	String detail() default "";
+}
