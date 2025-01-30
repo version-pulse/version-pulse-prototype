ab78b5e9af2c4cfe915548f80a3643facdd9b69e
kwonssshyeon <kwonsh4396@gmail.com>
2025-01-30 22:33:50 +0900

    Feat: insert record via notion integration

--- src/io/versionpulse/doc/apis/UpdateDatabase.java
+++ src/io/versionpulse/doc/apis/UpdateDatabase.java
@@ -1,16 +1,17 @@
 package io.versionpulse.doc.apis;
 
-import java.util.Arrays;
-import java.util.HashMap;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
-import io.versionpulse.doc.models.CreateModel;
 import io.versionpulse.doc.models.UpdateModel;
-import io.versionpulse.doc.models.UpdateModel.Parent;
-import io.versionpulse.doc.models.UpdateModel.Properties;
+import static io.versionpulse.doc.models.UpdateModel.*;
+import static io.versionpulse.doc.models.UpdateModel.Properties.*;
+
+import java.util.List;
+
+import static io.versionpulse.doc.models.UpdateModel.Child.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
@@ -30,11 +31,92 @@ public class UpdateDatabase {
         header.add("Authorization", "Bearer "+notionKey);
         header.add("Notion-Version", "2022-06-28");
         
-        UpdateModel body = new UpdateModel();
-        body.setParent(new Parent(databaseId));
-
+        Parent parent = Parent.builder()
+        		.database_id(databaseId)
+        		.build();
+        
+        Name name = Name.builder()
+        		.title(List.of(Name.Title.builder()
+    					.text(Name.Title.Text.builder()
+    							.content("API 이름")
+    							.build())
+    					.build()))
+        		.build();
+        
+        Description description = Description.builder()
+		        .rich_text(List.of(Description.RichText.builder()
+		                .text(Description.RichText.Text.builder()
+		                    .content("API 설명")
+		                    .build())
+		                .build()))
+		        .build();
+        
+        Path path = Path.builder()
+                .rich_text(List.of(Path.RichText.builder()
+                        .text(Path.RichText.Text.builder()
+                            .content("/sample/path")
+                            .build())
+                        .build()))
+                .build();
+        
+        Check check = Check.builder()
+                .checkbox(true)
+                .build();
+        
+        Method method = Method.builder()
+                .select(Method.Select.builder()
+                		.name("GET")
+                		.build())
+                .build();
+        
+        Properties properties = Properties.builder()
+        		.name(name)
+        		.description(description)
+        		.path(path)
+        		.check(check)
+        		.method(method)
+        		.build();
         
         
+        
+        Heading2 heading2 = Heading2.builder()
+        		.rich_text(List.of(Heading2.RichText.builder()
+        				.type("text")
+		                .text(Heading2.RichText.Text.builder()
+				                .content("Heading Text")
+				                .build())
+				        .build()))
+		    	.build();
+        
+        Paragraph paragraph = Paragraph.builder()
+                .rich_text(List.of(Paragraph.RichText.builder()
+                		.type("text")
+                        .text(Paragraph.RichText.Text.builder()
+                            .content("Paragraph content with link")
+                            .link(Paragraph.RichText.Text.Link.builder()
+                                .url("https://example.com")
+                                .build())
+                            .build())
+                        .build()))
+                    .build();
+        
+        Child child1 = Child.builder()
+        		.object("block")
+        		.type("heading_2")
+        		.heading_2(heading2)
+        		.build();
+        Child child2 = Child.builder()
+        		.object("block")
+        		.type("paragraph")
+        		.paragraph(paragraph)
+        		.build();
+        
+        UpdateModel body = UpdateModel.builder()
+        		.parent(parent)
+        		.properties(properties)
+        		.children(List.of(child1, child2))
+        		.build();
+                        
         
         HttpEntity<UpdateModel> request = new HttpEntity<>(body, header);
         RestTemplate restTemplate = new RestTemplate();
@@ -44,7 +126,6 @@ public class UpdateDatabase {
                 request,
                 String.class);
         
-        System.out.println(response);
 	}
 
 }
--- src/io/versionpulse/doc/models/UpdateModel.java
+++ src/io/versionpulse/doc/models/UpdateModel.java
@@ -1,26 +1,44 @@
 package io.versionpulse.doc.models;
 
 import java.util.List;
-import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class UpdateModel {
     public Parent parent;
     public Properties properties;
     public List<Child> children;
+    
+    @Override
+    public String toString() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "{}";
+        }
+    }
 
+    @Builder
     @AllArgsConstructor
     public static class Parent {
         public String database_id;
     }
 
+    @Builder
+    @AllArgsConstructor
     public static class Properties {
         public Name name;
         public Description description;
@@ -28,83 +46,127 @@ public class UpdateModel {
         public Check check;
         public Method method;
 
+        @Builder
+        @AllArgsConstructor
         public static class Name {
             public List<Title> title;
 
+            @Builder
+            @AllArgsConstructor
             public static class Title {
                 public Text text;
 
+                @Builder
+                @AllArgsConstructor
                 public static class Text {
                     public String content;
                 }
             }
         }
 
+        @Builder
+        @AllArgsConstructor
         public static class Description {
             public List<RichText> rich_text;
 
+            @Builder
+            @AllArgsConstructor
             public static class RichText {
                 public Text text;
 
+                @Builder
+                @AllArgsConstructor
                 public static class Text {
                     public String content;
                 }
             }
         }
 
+        @Builder
+        @AllArgsConstructor
         public static class Path {
             public List<RichText> rich_text;
 
+            @Builder
+            @AllArgsConstructor
             public static class RichText {
                 public Text text;
 
+                @Builder
+                @AllArgsConstructor
                 public static class Text {
                     public String content;
                 }
             }
         }
 
+        @Builder
+        @AllArgsConstructor
         public static class Check {
             public boolean checkbox;
         }
 
+        @Builder
+        @AllArgsConstructor
         public static class Method {
             public Select select;
-
+            
+            @Builder
+            @AllArgsConstructor
             public static class Select {
                 public String name;
             }
         }
     }
 
+    @Builder
+    @AllArgsConstructor
     public static class Child {
         public String object;
         public String type;
+        @JsonInclude(value = Include.NON_NULL)
         public Heading2 heading_2;
+        @JsonInclude(value = Include.NON_NULL)
         public Paragraph paragraph;
 
+        @Builder
+        @AllArgsConstructor
         public static class Heading2 {
             public List<RichText> rich_text;
 
+            @Builder
+            @AllArgsConstructor
             public static class RichText {
+            	public String type;
                 public Text text;
 
+                @Builder
+                @AllArgsConstructor
                 public static class Text {
                     public String content;
                 }
             }
         }
 
+        @Builder
+        @AllArgsConstructor
         public static class Paragraph {
             public List<RichText> rich_text;
 
+            @Builder
+            @AllArgsConstructor
             public static class RichText {
+            	public String type;
                 public Text text;
 
+                @Builder
+                @AllArgsConstructor
                 public static class Text {
                     public String content;
                     public Link link;
 
+                    @Builder
+                    @AllArgsConstructor
                     public static class Link {
                         public String url;
                     }
